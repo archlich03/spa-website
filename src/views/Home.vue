0d14666af552cfb61e679d7f8c9d7e213804a5fa
Archlich03 <github@stankunas.me>
2025-01-07 22:42:56 +0100

    fix: centering home page text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@ onMounted(() => {
 
 <template>
     <div class="container mx-auto max-w-[700px] flex flex-col items-center justify-center min-h-screen space-y-4">
-        <p class="text-left text-white text-bold text-6xl">
+        <p class="text-center text-white text-bold text-6xl">
             Hello, I'm Rokas. Welcome to my small corner of the internet!
         </p>
         <HomeButtons />
